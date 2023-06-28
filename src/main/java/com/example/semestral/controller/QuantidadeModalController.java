2684a3613c4ac0db90cb005c4ff6138f669355b6
Lucas Mickael Moratelli <lucasmickael.moratelli@gmail.com>
2023-06-27 21:39:56 -0300

    Resolvido problema de controller

--- src/main/java/com/example/semestral/controller/QuantidadeModalController.java
+++ src/main/java/com/example/semestral/controller/QuantidadeModalController.java
@@ -1,4 +1,24 @@
 package com.example.semestral.controller;
 
+import com.example.semestral.HelloApplication;
+import com.example.semestral.model.Produto;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class QuantidadeModalController {
+    @FXML
+    TextField quantidadeField;
+    @FXML
+    public void cancelar() {
+        HelloApplication.closeCurrentWindow();
+    }
+
+    @FXML
+    public void salvar() {
+        Produto novoProduto = new Produto();
+        try {
+            novoProduto.quantidade = Integer.parseInt(quantidadeField.getText());
+        }
+    }
 }
